060d32dbb778bcdc2aca8a8ef58aff824c409933
Rishab Hegde <reparadocs@pcp079840pcs.wireless.calpoly.edu>
2016-04-12 14:51:30 -0700

    fix

--- ShadeBot.py
+++ ShadeBot.py
@@ -1,10 +1,11 @@
-from flask import flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['GET'])
 def webhook():
   print request
+  return "<h1>Hello World</h1>"
 
 if __name__ == '__main__':
   app.run()
